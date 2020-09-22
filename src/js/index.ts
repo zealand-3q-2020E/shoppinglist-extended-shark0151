6d9a69375a05454ec4bf2f7af4d62dbd140b2ac7
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-09-22 12:18:45 +0200

    demo

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,40 @@
-//Write your Typescript code here
+//Write your Typescript code here
+
+let myElement:HTMLLIElement = <HTMLLIElement>document.getElementById("1");
+
+console.log(myElement);
+
+let myList:HTMLCollectionOf<HTMLLIElement> = <HTMLCollectionOf<HTMLLIElement>> document.getElementsByTagName("li");
+
+console.log("mylist consist of");
+console.log(myList);
+
+for (let index = 0; index < myList.length; index++) {
+        const element = myList[index];
+        console.log(element);
+        if(element.hasAttribute("class"))
+          {
+              if(element.getAttribute("class")=="unhealthy")
+              {
+                  element.setAttribute("class","healthy");
+              }
+              else
+              {
+                element.setAttribute("class","unhealthy");
+              }
+          }
+}
+
+let listunhelathy:HTMLUListElement = <HTMLUListElement>document.getElementById('listUnhealthy');
+
+let myTest:string = "Chips";
+
+let newLi:HTMLLIElement = <HTMLLIElement>document.createElement('li');
+newLi.setAttribute('class','unhealthy');
+newLi.setAttribute('id','16');
+let newText = document.createTextNode(myTest);
+newLi.appendChild(newText);
+
+listunhelathy.appendChild(newLi);
+
+    

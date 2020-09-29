fb7e392ef9a491530c7e03ca92120e8a8292c377
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-09-29 12:20:10 +0200

    eventlistner

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,21 @@
 //Write your Typescript code here
+
+document.getElementById("addButton").addEventListener('click',addGoods);
+let elementInput = document.getElementById('newGoods');
+
+function addGoods(e:MouseEvent){
+    console.log("In the addgoods function");
+    let inputText = (<HTMLInputElement>elementInput).value;
+    console.log("the input text is : " + inputText);
+
+    let listUnhealthy = document.getElementById("listUnhealthy");
+    let newLi = document.createElement('li');
+    let newText = document.createTextNode(inputText)
+    newLi.appendChild(newText);
+    newLi.setAttribute('class','unhealthy');
+
+listUnhealthy.appendChild(newLi);
+
+   
+
+}
